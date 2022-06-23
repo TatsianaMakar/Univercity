1dbddd4a17fd759d4cce442393ce3e040b49e7c8
Tatsiana Makar <7501480@gmail.com>
2022-06-23 21:15:23 +0300

    new commit idea2

--- lesson5/com/src/Main.java
+++ lesson5/com/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String []args){
-        System.out.printf("Таня");
+        System.out.println("Таня");
+        System.out.println("Макар");
     }
 }
