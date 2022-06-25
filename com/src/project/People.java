2a379d8744984759463f99c876c16c1eda6f7e07
Tatsiana Makar <7501480@gmail.com>
2022-06-25 11:14:06 +0300

    project version2

---
+++ com/src/project/People.java
@@ -0,0 +1,17 @@
+package project;
+
+public abstract class People {
+    private Adress adress;
+
+    public People (Adress adress){
+        this.adress=adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    public Adress getAdress() {
+        return this.adress;
+    }
+}
